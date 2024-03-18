8717185d4f0db89500e4d9487318c7ed109e383e
Dima Zboroshenko <>
2024-03-18 17:05:19 +0400

    add language weights code

---
+++ language_weights/src/main.rs
@@ -0,0 +1,78 @@
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+
+pub fn read_stdin() -> String {
+    let stdin = std::io::stdin();
+    let mut reader = BufReader::new(stdin.lock());
+    _read_stdin(&mut reader)
+}
+
+fn _read_stdin<R: BufRead>(reader: &mut R) -> String {
+    let mut line = String::new();
+    reader
+        .read_line(&mut line)
+        .expect("Failed to read input line");
+    line.trim().to_string()
+}
+
+pub fn prep_new_language(input: String) -> (String, i32) {
+    let mut arr = input.split(' ');
+    let res0 = arr.next().unwrap().to_string();
+    let res1 = arr.next().unwrap().parse::<i32>().unwrap();
+    (res0, res1)
+}
+
+fn init_languages() -> HashMap<String, i32> {
+    let mut languages = HashMap::new();
+    languages.insert("JavaScript".to_string(), 1995);
+    languages.insert("HTML/CSS".to_string(), 1990);
+    languages.insert("Python".to_string(), 1991);
+    languages.insert("SQL".to_string(), 1974);
+    languages.insert("TypeScript".to_string(), 2012);
+    languages.insert("Bash/Shell".to_string(), 1989);
+    languages.insert("Java".to_string(), 1995);
+    languages.insert("C#".to_string(), 2000);
+    languages.insert("C++".to_string(), 1985);
+    languages.insert("C".to_string(), 1972);
+    languages.insert("PHP".to_string(), 1995);
+    languages.insert("PowerShell".to_string(), 2006);
+    languages.insert("Go".to_string(), 2007);
+    languages.insert("Rust".to_string(), 2010);
+
+    languages
+}
+
+fn calculate_weights(years_active: &mut HashMap<String, i32>) -> Vec<(String, i32)> {
+    // Subtract the creation year from 2024 to get the number of years active.
+    for year in years_active.values_mut() {
+        *year = 2024 - *year;
+    }
+
+    let min_year = *years_active.values().min().unwrap_or(&0);
+    let max_year = *years_active.values().max().unwrap_or(&0);
+
+    let mut weights = Vec::new();
+
+    for (language, &year) in years_active.iter() {
+        let normalized_year = (year - min_year) as f64 / (max_year - min_year) as f64;
+        let weight = (normalized_year * 99.0) as i32 + 1; // weight between 1 and 100
+        weights.push((language.to_string(), weight));
+    }
+    weights.sort_by(|a, b| a.1.cmp(&b.1));
+
+    weights
+}
+
+fn main() {
+    let mut languages = init_languages();
+    println!("Enter the language with the year of it's fonudation:");
+    let input = read_stdin();
+    let (lang, year) = prep_new_language(input);
+    languages.insert(lang, year);
+    let weights = calculate_weights(&mut languages);
+
+    println!("Language weighing from 1-100 by age (1 is newest and 100 is oldest):");
+    for (language, weight) in &weights {
+        println!("{}: {}", language, weight);
+    }
+}
