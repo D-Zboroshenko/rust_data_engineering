0a40e491009006fd04107bf3058dfa2d9cdee5d2
Dima Zboroshenko <>
2024-03-18 13:46:01 +0400

    extend count frequency functionality

--- count_frequency/src/main.rs
+++ count_frequency/src/main.rs
@@ -2,8 +2,11 @@
 This example code counts the frequency of each number in the vector.
  */
 use std::collections::HashMap;
+use std::hash::Hash;
+use cli_utils::read_stdin;
+    
 
-fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
+fn logic<T: Hash + Eq>(numbers: Vec<T>) -> Vec<(T, u32)> {
     let mut frequencies = HashMap::new();
 
     for num in numbers {
@@ -17,6 +20,8 @@ fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
         result.push((num, frequency));
     }
 
+    result.sort_by(|a, b|a.1.cmp(&b.1));
+
     result
 }
 
@@ -28,4 +33,23 @@ fn main() {
         "The frequency of each number in the vector is: {:?}",
         result
     );
+
+    let words = vec!["some".to_string(), "words".to_string(), "are".to_string(), "just".to_string(), "words".to_string()];
+    let result = logic(words);
+    //print the results in a human readable format that explains what the result is.
+    println!(
+        "The frequency of each word in the vector is: {:?}",
+        result
+    );
+
+    println!("Please insert the words");
+    let input = read_stdin();
+    let words = input.split(" ").map(|s| s.to_string()).collect::<Vec<String>>();
+    let result = logic(words);
+    println!(
+        "The frequency of each word in the vector is: {:?}",
+        result
+    );
+
+
 }
